64ec3e31d2ce217a6723a1e6140ca30247066d84
augusstt06 <augusstt06@gmail.com>
2022-10-24 10:05:24 +0900

    Studying Use of Structure in C

--- data_Type/structure/structure6.c
+++ data_Type/structure/structure6.c
@@ -19,14 +19,14 @@ int main(void)
         scanf("%d", &list[i].number);
 
         printf("이름을 입력하세요 : ");
-        scanf("%s", &list[i].name);
+        scanf("%s", list[i].name);
 
         printf("학점을 입력하세요 : ");
         scanf("%lf", &list[i].grade);
     }
     for (i = 0; i < SIZE; i++)
     {
-        printf("학번 : %d, 이름 : %d, 학점 : %.1f\n", list[i].number, list[i].name, list[i].grade);
+        printf("학번 : %d, 이름 : %s, 학점 : %.1f\n", list[i].number, list[i].name, list[i].grade);
     }
 
     return 0;
