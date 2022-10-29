6f47d0aed8539f243348cdbad7e38d827d706586
augusstt06 <augusstt06@gmail.com>
2022-10-29 23:18:33 +0900

    Studying Dynamic Memory in C

--- dynamic_memory/dynamic_memory.c
+++ dynamic_memory/dynamic_memory.c
@@ -1,6 +1,19 @@
 #include <stdbool.h>
+#include <stdlib.h>
 
 int main(void)
 {
+    int *pi;
+    // malloc : 메모리를 할당
+    pi = (int *)malloc(sizeof(int));
+    if (pi == NULL)
+    {
+        printf("동적 메모리 할당에 실패하였습니다.\n");
+        exit(1);
+    }
+    *pi = 100;
+    printf("%d\n", *pi);
+    // 동적 메모리를 사용한 이후에는 무조건 해당 메모리를 빈한해야 한다.
+    free(pi);
     return 0;
 }
